531d83eb8aa801f25e0365735df7f4adcce59a07
Abhishek Rai <abhishek.rai@autodes>
2015-09-05 18:18:28 -0400

    Added Web Crawler python file Added function to search google based on a specified query

---
+++ web-crawler.py
@@ -0,0 +1,21 @@
+import re, urllib, json as m_json
+
+def fetch_google_results(passed_query):
+  urls = []
+  query = passed_query
+  query = urllib.urlencode ( { 'q' : query } )
+  response1 = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query + '&rsz=5' ).read()
+  response2 = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query + '&rsz=5&start=5' ).read()
+  json1 = m_json.loads ( response1 )
+  json2 = m_json.loads ( response2 )
+  results = json1 [ 'responseData' ] [ 'results' ] + json2 [ 'responseData' ] [ 'results' ]
+  for result in results:
+    urls.append(result['url'])
+  return urls
+
+def main():
+  search_results = fetch_google_results('dog')
+  print search_results
+
+if __name__ == '__main__':
+  main()
