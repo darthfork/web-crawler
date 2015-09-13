50edcd250a4e014ba32a223dc23d437fdb702d7a
Abhishek Rai <abhishek.rai@autodes>
2015-09-13 11:45:12 -0400

    Successful crawling

--- web-crawler.py
+++ web-crawler.py
@@ -1,8 +1,8 @@
 import re
 import urllib
 import json
-import ranking-function
-from ranking-function import BM25
+# import ranking-function
+# from ranking-function import BM25
 from BeautifulSoup import BeautifulSoup
 
 class WebCrawler:
@@ -26,34 +26,63 @@ class WebCrawler:
       self.urls.append((result['url'], 1)) #All google results are at depth 1 with google.com being at depth 0
 
   def normalize_url(self,url):
-    #URL normalization code
+    return url #URL normalization method skeleton
+
+
+  def print_urls(self):
+    print self.urls
 
   def parse_page(self,html_document,depth,query): #Extract all the links from the page and add them to the URLs list
     soup = BeautifulSoup(html_document)
+
     new_depth = depth+1
+
     for link in soup.findAll('a', attrs={'href': re.compile("^(http|https)://")}):
-      if normalize_url(link) not in self.visited:
+
+      if self.normalize_url(link) not in self.visited:
+
         href = link.get('href')
+
         #Filter the 'href' variable above to determine if it is relevant to the query
+
         self.urls.append((href,new_depth))
+
 
   def crawl(self):
     # Pop first URL from the URLs list and add it to the visited list and then parse them
-    while self.depth_reached <= 5: #initially trying till depth 5
-      url_tuple = urls.pop(0)
+
+    while self.depth_reached <= 2: #initially trying till depth 5
+      url_tuple = self.urls.pop(0)
+
       url = url_tuple[0]
+
       depth = url_tuple[1]
+
       self.depth_reached = depth
-      document = urllib.urlopen(url)
-      mime_type = document.info().gettype()
+      try:
+
+        document = urllib.urlopen(url)
+        mime_type = document.info().gettype()
+
+      except urllib.error as e:
+
+        error = e.read()
+        print error
+
       # Normalise the URL before inserting
-      self.visited[normalize_url(url)] = depth
-      if mime_type in self.valid_mime_types
-        parse_page(document,depth,self.query)
+
+      self.visited[self.normalize_url(url)] = depth
+
+      if mime_type in self.valid_mime_types:
+        self.parse_page(document,depth,self.query)
+
+    self.print_urls()
 
 def main():
-  query = raw_input ( 'Query: ' )
+  query = 'dog'#raw_input ( 'Query: ' )
   crawler = WebCrawler(query)
+  crawler.fetch_google_results()
+  crawler.crawl()
 
 
 if __name__ == '__main__':
